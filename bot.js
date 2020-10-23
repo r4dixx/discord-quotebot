fed4b0fd1871c8e1ef375c552b2aa6cee73f16a4
r4dixx <amael.sikel@gmail.com>
2020-10-23 18:45:13 +0200

    finish support for random quote

--- bot.js
+++ bot.js
@@ -7,31 +7,25 @@ const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('./quotes.db')
 db.run('CREATE TABLE IF NOT EXISTS quotes(quote text)');
 
-function randomQuote() {
-
-  let sql = `SELECT * FROM quotes WHERE quote IN (SELECT quote FROM quotes ORDER BY RANDOM() LIMIT 1)`;
-
-  db.all(sql, [], (err, rows) => {
-    if (err) {
-      throw err;
-    }
-    rows.forEach((row) => {
-      console.log(row.quote);
-      return row.quote
-    });
-  })
-
-};
-
-console.log(randomQuote());
-
 var Discord = require("discord.js");
 var bot = new Discord.Client();
 var trigger = prefix + command
 
 bot.on("message", (message) => {
   if (message.content == trigger) {
-    message.channel.send(randomQuote());
+
+    let sql = `SELECT * FROM quotes WHERE quote IN (SELECT quote FROM quotes ORDER BY RANDOM() LIMIT 1)`;
+
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        throw err;
+      }
+      rows.forEach((row) => {
+        console.log("quote displayed: " + row.quote);
+        message.channel.send(row.quote)
+      });
+    })
+
   } else if (message.content.startsWith(trigger)) {
 
     var quoteClean = message.content.replace(trigger, "").substring(1)
@@ -40,7 +34,7 @@ bot.on("message", (message) => {
       if (err) {
         return console.log(err.message);
       }
-      console.log(`A row has been inserted with rowid ${this.lastID}`);
+      console.log(`quote saved: ` + quoteClean);
     });
 
     message.channel.send("**Citation enregistrée → **" + quoteClean);
