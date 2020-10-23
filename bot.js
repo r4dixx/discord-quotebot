ba33d70b53860a2fbf3c030c0eff029aeb0f7cc2
r4dixx <amael.sikel@gmail.com>
2020-10-23 14:33:53 +0200

    Prepare quote save

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ var InfiniteLoop = require('infinite-loop');
 var il = new InfiniteLoop;
 
 function randomQuote() {
-	return quotes[Math.floor(Math.random() * quotes.length)];
+  return quotes[Math.floor(Math.random() * quotes.length)];
 };
 il.add(randomQuote, []);
 
@@ -17,15 +17,23 @@ console.log(randomQuote());
 
 var Discord = require("discord.js");
 var bot = new Discord.Client();
+var trigger = prefix + command
+
 bot.on("message", (message) => {
-  if (message.content.startsWith(prefix + command)) {
+  if (message.content == trigger) {
     message.channel.send(randomQuote());
+  } else if (message.content.startsWith(trigger)) {
+    // save quote here
+    message.channel.send("**Citation enregistrée → **" + message.content.replace(trigger, "").substring(1));
   }
 });
 
+var ping = prefix + "ping"
 bot.on("message", (message) => {
-  if (message.content.startsWith(prefix + "ping")) {
-    message.channel.send("Pong!");
+  if (message.content == ping) {
+    message.channel.send("pong");
+  } else if (message.content.startsWith(ping)) {
+    message.channel.send(message.content.replace(ping, ""));
   }
 });
 
