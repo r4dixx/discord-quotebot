d44ab29e8ceff8ca343317fcf19e9540775d3eba
r4dixx <amael.sikel@gmail.com>
2020-10-25 22:02:24 +0100

    Review strings and other stuff

--- bot.js
+++ bot.js
@@ -1,74 +1,69 @@
-const SQLITE = require("sqlite3").verbose();
-const DISCORD = require("discord.js");
-const CONFIG = require("./config.json");
+const SQLITE = require('sqlite3').verbose();
+const DISCORD = require('discord.js');
+const CONFIG = require('./config.json');
+
+const CLIENT = new DISCORD.Client();
+
 const PREFIX = CONFIG.prefix;
 const COMMAND_QUOTE = CONFIG.command_quote
 const COMMAND_HELP = CONFIG.command_help;
-const COMMAND_PING = CONFIG.command_ping;
 const FEEDBACK_CONFIRM = CONFIG.feedback_confirm;
 
-const client = new DISCORD.Client();
 let db = null
 
 openDb()
-db.run("CREATE TABLE IF NOT EXISTS quotes(quote text)");
+db.run('CREATE TABLE IF NOT EXISTS quotes(quote text)');
 closeDb()
 
-client.on("message", (message) => {
+CLIENT.on('message', (message) => {
   var msg = message.content
   var trigger_quote = PREFIX + COMMAND_QUOTE
   var trigger_help = PREFIX + COMMAND_HELP
-  var trigger_ping = PREFIX + COMMAND_PING
 
   if (msg == trigger_quote) {
     openDb()
-    db.all("SELECT * FROM quotes WHERE quote IN (SELECT quote FROM quotes ORDER BY RANDOM() LIMIT 1)", [], (err, rows) => {
+    db.all('SELECT * FROM quotes WHERE quote IN (SELECT quote FROM quotes ORDER BY RANDOM() LIMIT 1)', [], (err, rows) => {
       if (err) {
         throw err;
       }
       rows.forEach((row) => {
-        console.log("quote displayed: " + row.quote);
+        console.log(`Quote displayed: ${row.quote}`);
         message.channel.send(row.quote)
       });
     })
     closeDb()
   } else if (msg.startsWith(trigger_quote)) {
-    var quoteClean = msg.replace(trigger_quote, "").substring(1)
+    var quoteClean = msg.replace(trigger_quote, ``).substring(1)
     openDb()
-    db.run("INSERT INTO quotes(quote) VALUES(?)", quoteClean, function(err) {
+    db.run('INSERT INTO quotes(quote) VALUES(?)', quoteClean, function(err) {
       if (err) {
         return console.log(err.message);
       }
-      console.log("quote saved: " + quoteClean);
+      console.log(`Quote saved: ${quoteClean}`);
     });
     closeDb()
-    message.channel.send("_" + FEEDBACK_CONFIRM + "_" + "\n" + quoteClean);
+    message.channel.send(`${FEEDBACK_CONFIRM}\n${quoteClean}`);
   } else if (msg.startsWith(trigger_help)) {
-    message.channel.send(
-      CONFIG.help_add + " " + "`" + trigger_quote + "`" + " " + "`" + CONFIG.required_formatting + "`" + "\n" +
-      CONFIG.help_get + " " + "`" + trigger_quote + "`" + "\n" +
-      CONFIG.help_ping + " " + "`" + trigger_ping + "`" + "\n" +
-      CONFIG.help_self + " " + "`" + trigger_help + "`" + "\n"
-    )
-    console.log("help displayed");
-  } else if (msg == trigger_ping) {
-    message.reply("pong");
-    console.log("pong");
-  } else if (msg.startsWith(trigger_ping)) {
-    var test_feedback = msg.replace(trigger_ping, "").substring(1)
-    message.reply(test_feedback);
-    console.log(COMMAND_PING + " : " + test_feedback);
+    message.channel.send(`${CONFIG.help_add} \`${trigger_quote}\` \`${CONFIG.help_add_formatting}\`\n${CONFIG.help_display} \`${trigger_quote}\`\n${CONFIG.help_self} \`${trigger_help}\``)
+    console.log("Help displayed");
+  } else if (msg == "/ping") {
+    message.reply("Pong");
+    console.log("Pong");
+  } else if (msg.startsWith("/ping")) {
+    var pong = msg.replace("/ping", "").substring(1)
+    message.reply(`Pong: ${pong}`);
+    console.log(`Pong: ${pong}`);
   }
 });
 
-client.login(CONFIG.token);
+CLIENT.login(CONFIG.token);
 
 function openDb() {
   db = new SQLITE.Database('./quotes.db', (err) => {
     if (err) {
       return console.error(err.message);
     }
-    console.log('Connected to SQlite database.');
+    console.log("Connected to SQlite database");
   });
 }
 
@@ -77,6 +72,6 @@ function closeDb() {
     if (err) {
       return console.error(err.message);
     }
-    console.log('Close the database connection.');
+    console.log("Closed database connection");
   });
 }
