6bf070c7a798d222cf93bab0420cbb02fb12b1df
r4dixx <amael.sikel@gmail.com>
2020-11-22 00:37:56 +0100

    Add user output

--- main.js
+++ main.js
@@ -15,6 +15,8 @@ getClient().on('message', (message) => {
   const PREFIX = CONFIG.prefix;
   const CONFIG_COMMAND = CONFIG.command;
   const CONFIG_FEEDBACK = CONFIG.feedback;
+  const CONFIG_FEEDBACK_SUCCESS = CONFIG_FEEDBACK.success;
+  const CONFIG_FEEDBACK_ERROR = CONFIG_FEEDBACK.error;
 
   const COMMAND_GET = PREFIX + CONFIG_COMMAND.get;
   const COMMAND_ADD = PREFIX + CONFIG_COMMAND.add;
@@ -29,7 +31,7 @@ getClient().on('message', (message) => {
 
   function sendRandomQuote() {
     queryQuoteRandom().then(function(result) {
-      message.channel.send(result || CONFIG_FEEDBACK.error.get);
+      message.channel.send(result || CONFIG_FEEDBACK_ERROR.get);
     });
   }
 
@@ -40,12 +42,14 @@ getClient().on('message', (message) => {
   }
 
   function deleteQuote() {
-    const BOT_ADMIN_IDS = (require('./config_private.json').botAdminIds);
+    const BOT_ADMIN_IDS = require('./config_private.json').botAdminIds;
     const AUTHOR_ID = message.author.id;
     if (BOT_ADMIN_IDS.includes(AUTHOR_ID)) {
       console.log(`Success: author id ${AUTHOR_ID} is a bot admin`);
+      message.channel.send(CONFIG_FEEDBACK_SUCCESS.delete);
     } else {
       console.log(`Error: author id ${AUTHOR_ID} is not a bot admin. Bot admins are ${BOT_ADMIN_IDS}. Aborting...`);
+      message.channel.send(CONFIG_FEEDBACK_ERROR.delete);
     }
   }
 
