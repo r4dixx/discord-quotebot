d9f0bfe3f8a4e2b0156b59fecb5cdfc1583630fa
r4dixx <amael.sikel@gmail.com>
2020-11-23 15:15:05 +0100

    Rename functions

--- dbQueries.js
+++ dbQueries.js
@@ -4,33 +4,33 @@ require('./dbHelper.js')();
 
 module.exports = function() {
 
-  createTableIfNecessary = function() {
+  dbCreateTableIfNecessary = function() {
     const DB_PATH = './quotes.db';
     if (require('fs').existsSync(DB_PATH)) console.log(`File ${DB_PATH} exists. Moving on`);
     else {
       console.log(`${DB_PATH} not found, creating...`);
-      openDb();
-      getDb().run('CREATE TABLE IF NOT EXISTS quotes(quote TEXT)', (err) => {
+      dbOpen();
+      dbGet().run('CREATE TABLE IF NOT EXISTS quotes(quote TEXT)', (err) => {
         if (err) throw err;
         console.log('Quotes table created');
       });
-      closeDb();
+      dbClose();
     }
   };
 
-  insertQuote = function(quote) {
-    openDb();
-    getDb().run('INSERT INTO quotes(quote) VALUES(?)', quote, (err) => {
+  dbInsertQuote = function(quote) {
+    dbOpen();
+    dbGet().run('INSERT INTO quotes(quote) VALUES(?)', quote, (err) => {
       if (err) throw err;
       console.log(`Quote saved: ${quote}`);
     });
-    closeDb();
+    dbClose();
   };
 
-  queryQuoteRandom = function() {
+  dbQueryQuoteRandom = function() {
     return new Promise(function(resolve, reject) {
-      openDb();
-      getDb().get('SELECT quote FROM quotes ORDER BY RANDOM() LIMIT 1', (err, row) => {
+      dbOpen();
+      dbGet().get('SELECT quote FROM quotes ORDER BY RANDOM() LIMIT 1', (err, row) => {
         if (err) throw err;
         else if (row == null || row.quote == null) {
           console.log('Cannot get random quote. No quote found in database');
@@ -40,18 +40,18 @@ module.exports = function() {
           resolve(row.quote);
         }
       });
-      closeDb();
+      dbClose();
     });
   };
 
   // TODO check if there is a row
-  deleteQuoteLast = function() {
-    openDb();
-    getDb().run('DELETE FROM quotes WHERE rowid = (SELECT MAX(rowid) FROM quotes)', (err) => {
+  dbDeleteQuoteLast = function() {
+    dbOpen();
+    dbGet().run('DELETE FROM quotes WHERE rowid = (SELECT MAX(rowid) FROM quotes)', (err) => {
       if (err) throw err;
       else console.log('Last quote deleted successfully');
     });
-    closeDb();
+    dbClose();
   };
 
 };
