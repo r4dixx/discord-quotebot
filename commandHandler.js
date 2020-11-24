213cee9e7c7c393dd7b42bb75c9558df88e7a8b1
r4dixx <amael.sikel@gmail.com>
2020-11-24 13:03:25 +0100

    Move commands to a separate file

---
+++ commandHandler.js
@@ -0,0 +1,58 @@
+/*jshint esversion: 6 */
+
+require('./dbQueries.js')();
+
+module.exports = function() {
+
+  const CONFIG = require('./config.json');
+  const CONFIG_COMMAND = CONFIG.trigger.command;
+  const CONFIG_FEEDBACK = CONFIG.feedback;
+  const CONFIG_FEEDBACK_SUCCESS = CONFIG_FEEDBACK.success;
+  const CONFIG_FEEDBACK_ERROR = CONFIG_FEEDBACK.error;
+
+  getClient().on('message', (message) => {
+
+    sendQuoteRandom = function() {
+      dbQueryItemRandom().then(function(result) {
+        message.channel.send(result || CONFIG_FEEDBACK_ERROR.get);
+      });
+    };
+
+    addQuote = function() {
+      let quote = message.content.replace(`${buildTrigger(CONFIG_COMMAND.add)} `, '');
+      dbInsertItem(quote);
+      message.channel.send(`${CONFIG_FEEDBACK_SUCCESS.add}\n${quote}`);
+    };
+
+    deleteQuoteLast = function() {
+      dbDeleteItemLast().then(function(result) {
+        if (result != null) message.channel.send(`${CONFIG_FEEDBACK_SUCCESS.delete}\n${result}`);
+        else message.channel.send(CONFIG_FEEDBACK_ERROR.delete);
+      });
+    };
+
+    // function deleteQuote() {
+    //   if (getRights(message.author.id) == true) {
+    //
+    //   } else message.channel.send(CONFIG_FEEDBACK_ERROR.rights);
+    // }
+
+    sendHelp = function() {
+      const CONFIG_HELP = CONFIG_FEEDBACK.help;
+      const CONFIG_HELP_TITLE = CONFIG_HELP.title;
+      const CONFIG_HELP_COMMANDS = CONFIG_HELP.commands;
+      const CONFIG_HELP_SHOW = CONFIG_HELP.show;
+
+      message.channel.send(`${CONFIG_HELP.about}\n\n${CONFIG_HELP_TITLE.user}\n• ${CONFIG_HELP_COMMANDS.get} → \`${buildTrigger(CONFIG_COMMAND.get)}\`\n• ${CONFIG_HELP_COMMANDS.add} → \`${buildTrigger(CONFIG_COMMAND.add)}\` \`${CONFIG_HELP_COMMANDS.add_format}\`\n\n${CONFIG_HELP_TITLE.admin}\n• ${CONFIG_HELP_COMMANDS.delete} → \`${buildTrigger(CONFIG_COMMAND.delete)}\`\n\n${CONFIG_HELP_TITLE.self}\n• ${CONFIG_HELP_SHOW.with_command} \`${buildTrigger(CONFIG_COMMAND.help)}\` ${CONFIG_HELP_SHOW.with_mention} <@!${getClient().user.id}>`);
+
+      console.log('Help displayed');
+    };
+
+    sendPong = function() {
+      console.log(`Sent \"pong\" to ${message.author.username}`);
+      message.reply('pong');
+    };
+
+  });
+
+};
--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 require('./discordHelper.js')();
 require('./dbQueries.js')();
+require('./commandHandler.js')();
 
 login();
 
@@ -9,76 +10,26 @@ dbCreateTableIfNecessary();
 
 getClient().on('message', (message) => {
 
-  const THIS_AUTHOR_ID = getClient().user.id;
-  const MESSAGE_AUTHOR_ID = message.author.id;
-
-  if (MESSAGE_AUTHOR_ID == THIS_AUTHOR_ID) return;
+  if (message.author.id == getClient().user.id) return;
 
   const CONFIG = require('./config.json');
   const CONFIG_COMMAND = CONFIG.trigger.command;
-  const CONFIG_FEEDBACK = CONFIG.feedback;
-  const CONFIG_FEEDBACK_SUCCESS = CONFIG_FEEDBACK.success;
-  const CONFIG_FEEDBACK_ERROR = CONFIG_FEEDBACK.error;
 
-  const MESSAGE_CONTENT = message.content;
-
-  if (MESSAGE_CONTENT === buildTrigger(CONFIG_COMMAND.get))
+  if (message.content === buildTrigger(CONFIG_COMMAND.get))
     sendQuoteRandom();
-  else if (MESSAGE_CONTENT.startsWith(buildTrigger(CONFIG_COMMAND.add) + ' '))
+  else if (message.content.startsWith(buildTrigger(CONFIG_COMMAND.add) + ' '))
     addQuote();
-  else if (MESSAGE_CONTENT == buildTrigger(CONFIG_COMMAND.delete) && hasRights()) deleteQuoteLast();
-  // else if (MESSAGE_CONTENT.startsWith(buildTrigger(CONFIG_COMMAND.get)  + ' ' + '#'')) deleteQuote();
-  else if (MESSAGE_CONTENT === buildTrigger(CONFIG_COMMAND.help) || message.mentions.members.has(THIS_AUTHOR_ID))
+  else if (message.content == buildTrigger(CONFIG_COMMAND.delete) && hasRights()) deleteQuoteLast();
+  // else if (message.content.startsWith(buildTrigger(CONFIG_COMMAND.get)  + ' ' + '#'')) deleteQuote();
+  else if (message.content === buildTrigger(CONFIG_COMMAND.help) || message.mentions.members.has(getClient().user.id))
     sendHelp();
-  else if (MESSAGE_CONTENT === buildTrigger('ping'))
+  else if (message.content === buildTrigger('ping'))
     sendPong();
 
-
-  function sendQuoteRandom() {
-    dbQueryItemRandom().then(function(result) {
-      message.channel.send(result || CONFIG_FEEDBACK_ERROR.get);
-    });
-  }
-
-  function addQuote() {
-    let quote = MESSAGE_CONTENT.replace(`${buildTrigger(CONFIG_COMMAND.add)} `, '');
-    dbInsertItem(quote);
-    message.channel.send(`${CONFIG_FEEDBACK_SUCCESS.add}\n${quote}`);
-  }
-
-  function deleteQuoteLast() {
-    dbDeleteItemLast().then(function(result) {
-      if (result != null) message.channel.send(`${CONFIG_FEEDBACK_SUCCESS.delete}\n${result}`);
-      else message.channel.send(CONFIG_FEEDBACK_ERROR.delete);
-    });
-  }
-
-  // function deleteQuote() {
-  //   if (getRights(MESSAGE_AUTHOR_ID) == true) {
-  //
-  //   } else message.channel.send(CONFIG_FEEDBACK_ERROR.rights);
-  // }
-
-  function sendHelp() {
-    const CONFIG_HELP = CONFIG_FEEDBACK.help;
-    const CONFIG_HELP_TITLE = CONFIG_HELP.title;
-    const CONFIG_HELP_COMMANDS = CONFIG_HELP.commands;
-    const CONFIG_HELP_SHOW = CONFIG_HELP.show;
-
-    message.channel.send(`${CONFIG_HELP.about}\n\n${CONFIG_HELP_TITLE.user}\n• ${CONFIG_HELP_COMMANDS.get} → \`${buildTrigger(CONFIG_COMMAND.get)}\`\n• ${CONFIG_HELP_COMMANDS.add} → \`${buildTrigger(CONFIG_COMMAND.add)}\` \`${CONFIG_HELP_COMMANDS.add_format}\`\n\n${CONFIG_HELP_TITLE.admin}\n• ${CONFIG_HELP_COMMANDS.delete} → \`${buildTrigger(CONFIG_COMMAND.delete)}\`\n\n${CONFIG_HELP_TITLE.self}\n• ${CONFIG_HELP_SHOW.with_command} \`${buildTrigger(CONFIG_COMMAND.help)}\` ${CONFIG_HELP_SHOW.with_mention} <@!${THIS_AUTHOR_ID}>`);
-
-    console.log('Help displayed');
-  }
-
-  function sendPong() {
-    console.log(`Sent \"pong\" to ${message.author.username}`);
-    message.reply('pong');
-  }
-
   function hasRights() {
-    if (getRights(MESSAGE_AUTHOR_ID)) return true;
+    if (getRights(message.author.id)) return true;
     else {
-      message.channel.send(CONFIG_FEEDBACK_ERROR.rights);
+      message.channel.send(CONFIG.feedback.error.rights);
       return false;
     }
   }
