4956c943fe233e7b1e1d6030de2e27994871e119
r4dixx <amael.sikel@gmail.com>
2020-11-24 19:56:43 +0100

    Fix delete item

--- dbQueries.js
+++ dbQueries.js
@@ -48,16 +48,16 @@ module.exports = function() {
     return new Promise(function(resolve, reject) {
       let query;
       dbOpen();
-      if (quoteForDeletion != null) query = `SELECT quote FROM quotes WHERE quote = '${quoteForDeletion}'`;
+      if (quoteForDeletion != null) query = 'SELECT quote FROM quotes WHERE quote = ?';
       else query = 'SELECT rowid, quote FROM quotes ORDER BY rowid DESC LIMIT 1';
-      dbGet().get(query, (err, row) => {
+      dbGet().get(query, quoteForDeletion, (err, row) => {
         if (err) return console.error(err.message);
         if (row == null || row.quote == null) {
           console.log('Error: Cannot get quote for deletion. Not found in database');
           resolve(null);
         } else {
           if (quoteForDeletion == null) quoteForDeletion = row.quote;
-          dbGet().run(`DELETE FROM quotes WHERE quote = '${quoteForDeletion}'`, function(err) {
+          dbGet().run('DELETE FROM quotes WHERE quote = ?', quoteForDeletion, function(err) {
             if (err) return console.error(err.message);
             console.log(`Deleted quote: ${quoteForDeletion}`);
           });
