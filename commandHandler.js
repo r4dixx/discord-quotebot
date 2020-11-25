592d5fd901742f9c7f6feb10e5a80effe30e3775
r4dixx <amael.sikel@gmail.com>
2020-11-25 13:13:05 +0100

    Review formatting

--- commandHandler.js
+++ commandHandler.js
@@ -29,8 +29,17 @@ module.exports = function() {
       dbUpdateLast(updatedQuote).then(function(result) {
         const CONFIG_FEEDBACK_SUCCESS_UPDATE = CONFIG_FEEDBACK_SUCCESS.update;
         const CONFIG_FEEDBACK_ERROR_UPDATE = CONFIG_FEEDBACK_ERROR.update;
-        if (result != null) message.channel.send(`${CONFIG_FEEDBACK_SUCCESS_UPDATE.title}\n${CONFIG_FEEDBACK_SUCCESS_UPDATE.before}\n${result}\n${CONFIG_FEEDBACK_SUCCESS_UPDATE.after}\n${updatedQuote}`);
-        else {
+        if (result != null) {
+
+          message.channel.send(`
+${CONFIG_FEEDBACK_SUCCESS_UPDATE.title}
+${CONFIG_FEEDBACK_SUCCESS_UPDATE.before}
+${result}
+${CONFIG_FEEDBACK_SUCCESS_UPDATE.after}
+${updatedQuote}
+          `);
+
+        } else {
           let errorMessage;
           if (quote != null) errorMessage = CONFIG_FEEDBACK_ERROR_UPDATE.item;
           else errorMessage = CONFIG_FEEDBACK_ERROR_UPDATE.last;
@@ -69,7 +78,24 @@ module.exports = function() {
 
       const CONFIG_COMMAND = CONFIG.trigger.command;
 
-      message.channel.send(`${CONFIG_HELP.about}\n\n${CONFIG_HELP_USER.title}\n• ${CONFIG_HELP_USER.get} → \`${buildTrigger(CONFIG_COMMAND.get)}\`\n• ${CONFIG_HELP_USER_INSERT.item} → \`${buildTrigger(CONFIG_COMMAND.insert)}\` \`${CONFIG_HELP_USER_INSERT.format}\`\n\n${CONFIG_HELP_ADMIN.title}\n• ${CONFIG_HELP_ADMIN_UPDATE.last} → \`${buildTrigger(CONFIG_COMMAND.update)}\` \`${CONFIG_HELP_ADMIN_UPDATE_FORMAT.after}\`\n• ${CONFIG_HELP_ADMIN_UPDATE.item} → \`${buildTrigger(CONFIG_COMMAND.update)}\` \`${CONFIG_HELP_ADMIN_UPDATE_FORMAT.before}\` \`${CONFIG_HELP_ADMIN_UPDATE_FORMAT.after}\`\n• ${CONFIG_HELP_ADMIN_DELETE.last} → \`${buildTrigger(CONFIG_COMMAND.delete)}\`\n• ${CONFIG_HELP_ADMIN_DELETE.item} → \`${buildTrigger(CONFIG_COMMAND.delete)}\` \`${CONFIG_HELP_ADMIN_DELETE.format}\`\n\n${CONFIG_HELP_SELF.title}\n• ${CONFIG_HELP_SELF_PREFIX.command} \`${buildTrigger(CONFIG_COMMAND.help)}\` ${CONFIG_HELP_SELF_PREFIX.mention} <@!${getClient().user.id}>`);
+      message.channel.send(`
+${CONFIG_HELP.about}
+
+${CONFIG_HELP_USER.title}
+• ${CONFIG_HELP_USER.get} → \`${buildTrigger(CONFIG_COMMAND.get)}\`
+• ${CONFIG_HELP_USER_INSERT.item} → \`${buildTrigger(CONFIG_COMMAND.insert)}\` \`${CONFIG_HELP_USER_INSERT.format}\`
+
+${CONFIG_HELP_ADMIN.title}
+• ${CONFIG_HELP_ADMIN_UPDATE.title}
+    ‣ ${CONFIG_HELP_ADMIN_UPDATE.last} → \`${buildTrigger(CONFIG_COMMAND.update)}\` \`${CONFIG_HELP_ADMIN_UPDATE_FORMAT.after}\`
+    ‣ ${CONFIG_HELP_ADMIN_UPDATE.item} → \`${buildTrigger(CONFIG_COMMAND.update)}\` \`${CONFIG_HELP_ADMIN_UPDATE_FORMAT.before}\` \`${CONFIG_HELP_ADMIN_UPDATE_FORMAT.after}\`
+• ${CONFIG_HELP_ADMIN_DELETE.title}
+    ‣ ${CONFIG_HELP_ADMIN_DELETE.last} → \`${buildTrigger(CONFIG_COMMAND.delete)}\`
+    ‣ ${CONFIG_HELP_ADMIN_DELETE.item} → \`${buildTrigger(CONFIG_COMMAND.delete)}\` \`${CONFIG_HELP_ADMIN_DELETE.format}\`
+
+${CONFIG_HELP_SELF.title}
+• ${CONFIG_HELP_SELF_PREFIX.command} \`${buildTrigger(CONFIG_COMMAND.help)}\` ${CONFIG_HELP_SELF_PREFIX.mention} <@!${getClient().user.id}>
+      `);
 
       console.log('Help displayed');
     };
