581c6b7f9f0531c451777265db34814fe20620c4
r4dixx <amael.sikel@gmail.com>
2020-11-25 23:43:10 +0100

    Misc model improvements

--- triggersUser.js
+++ triggersUser.js
@@ -26,16 +26,15 @@ module.exports = function() {
       const CONFIG_HELP = CONFIG_FEEDBACK.help;
 
       const CONFIG_HELP_USER = CONFIG_HELP.user;
-      const CONFIG_HELP_USER_INSERT = CONFIG_HELP_USER.insert;
 
       const CONFIG_HELP_ADMIN = CONFIG_HELP.admin;
       const CONFIG_HELP_ADMIN_UPDATE = CONFIG_HELP_ADMIN.update;
-      const CONFIG_HELP_ADMIN_UPDATE_FORMAT = CONFIG_HELP_ADMIN_UPDATE.formats;
       const CONFIG_HELP_ADMIN_DELETE = CONFIG_HELP_ADMIN.delete;
-      const CONFIG_HELP_ADMIN_DELETE_FORMAT = CONFIG_HELP_ADMIN_DELETE.format;
 
       const CONFIG_HELP_SELF = CONFIG_HELP.self;
       const CONFIG_HELP_SELF_PREFIXES = CONFIG_HELP_SELF.prefixes;
+
+      const CONFIG_HELP_FORMATS = CONFIG_HELP.formats;
 
       const CONFIG_COMMANDS = CONFIG.trigger.commands;
       const CONFIG_COMMANDS_UPDATE = CONFIG_COMMANDS.update;
@@ -45,15 +44,15 @@ ${CONFIG_HELP.about}
 
 ${CONFIG_HELP_USER.title}
 • ${CONFIG_HELP_USER.get} → \`${buildTrigger(CONFIG_COMMANDS.get)}\`
-• ${CONFIG_HELP_USER_INSERT.item} → \`${buildTrigger(CONFIG_COMMANDS.insert)}\` \`${CONFIG_HELP_USER_INSERT.format}\`
+• ${CONFIG_HELP_USER.insert} → \`${buildTrigger(CONFIG_COMMANDS.insert)}\` \`${CONFIG_HELP_FORMATS.current}\`
 
 ${CONFIG_HELP_ADMIN.title}
 • ${CONFIG_HELP_ADMIN_UPDATE.title}
-    ‣ ${CONFIG_HELP_ADMIN_UPDATE.last} → \`${buildTrigger(CONFIG_COMMANDS_UPDATE.command)}\` \`${CONFIG_HELP_ADMIN_UPDATE_FORMAT.new}\`
-    ‣ ${CONFIG_HELP_ADMIN_UPDATE.item} → \`${buildTrigger(CONFIG_COMMANDS_UPDATE.command)}\` \`${CONFIG_COMMANDS_UPDATE.current}${CONFIG_HELP_ADMIN_UPDATE_FORMAT.current}\` \`${CONFIG_COMMANDS_UPDATE.new}${CONFIG_HELP_ADMIN_UPDATE_FORMAT.new}\`
+    ‣ ${CONFIG_HELP_ADMIN_UPDATE.last} → \`${buildTrigger(CONFIG_COMMANDS_UPDATE.command)}\` \`${CONFIG_HELP_FORMATS.new}\`
+    ‣ ${CONFIG_HELP_ADMIN_UPDATE.item} → \`${buildTrigger(CONFIG_COMMANDS_UPDATE.command)}\` \`${CONFIG_COMMANDS_UPDATE.current}${CONFIG_HELP_FORMATS.current}\` \`${CONFIG_COMMANDS_UPDATE.new}${CONFIG_HELP_FORMATS.new}\`
 • ${CONFIG_HELP_ADMIN_DELETE.title}
     ‣ ${CONFIG_HELP_ADMIN_DELETE.last} → \`${buildTrigger(CONFIG_COMMANDS.delete)}\`
-    ‣ ${CONFIG_HELP_ADMIN_DELETE.item} → \`${buildTrigger(CONFIG_COMMANDS.delete)}\` \`${CONFIG_HELP_ADMIN_DELETE.format}\`
+    ‣ ${CONFIG_HELP_ADMIN_DELETE.item} → \`${buildTrigger(CONFIG_COMMANDS.delete)}\` \`${CONFIG_HELP_FORMATS.delete}\`
 
 ${CONFIG_HELP_SELF.title}
 • ${CONFIG_HELP_SELF_PREFIXES.command} \`${buildTrigger(CONFIG_COMMANDS.help)}\` ${CONFIG_HELP_SELF_PREFIXES.mention} <@!${getClient().user.id}>
