27a935b4bec6afa9f01b2d512a5633084755ffe6
r4dixx <amael.sikel@gmail.com>
2020-11-25 17:07:47 +0100

    Review formatting and model

--- main.js
+++ main.js
@@ -13,21 +13,22 @@ getClient().on('message', (message) => {
   if (message.author.id == getClient().user.id) return;
 
   const CONFIG = require('./config.json');
-  const CONFIG_COMMAND = CONFIG.trigger.command;
+  const CONFIG_COMMANDS = CONFIG.trigger.commands;
+  const CONFIG_COMMANDS_UPDATE = CONFIG_COMMANDS.update;
 
-  if (message.content === buildTrigger(CONFIG_COMMAND.get))
+  if (message.content === buildTrigger(CONFIG_COMMANDS.get))
     sendQuoteRandom();
-  else if (message.content.startsWith(buildTrigger(CONFIG_COMMAND.insert) + ' '))
-    insertQuote(message.content.replace(`${buildTrigger(CONFIG_COMMAND.insert)} `, ''));
-  else if (message.content.startsWith(buildTrigger(CONFIG_COMMAND.update) + ' ') && userIsAdmin())
-    updateQuoteLast(message.content.replace(`${buildTrigger(CONFIG_COMMAND.update)} `, ''));
-  // else if (message.content === buildTrigger(CONFIG_COMMAND.update) && userIsAdmin())
+  else if (message.content.startsWith(buildTrigger(CONFIG_COMMANDS.insert) + ' '))
+    insertQuote(message.content.replace(`${buildTrigger(CONFIG_COMMANDS.insert)} `, ''));
+  else if (message.content.startsWith(buildTrigger(CONFIG_COMMANDS_UPDATE.command) + ' ') && userIsAdmin())
+    updateQuoteLast(message.content.replace(`${buildTrigger(CONFIG_COMMANDS_UPDATE.command)} `, ''));
+  // else if (message.content.startsWith(buildTrigger(CONFIG_COMMANDS_UPDATE.command) + ' ') && message.content.includes(CONFIG_COMMANDS_UPDATE.current) && message.content.includes(CONFIG_COMMANDS_UPDATE.new) && userIsAdmin())
   //   updateQuote();
-  else if (message.content === buildTrigger(CONFIG_COMMAND.delete) && userIsAdmin())
+  else if (message.content === buildTrigger(CONFIG_COMMANDS.delete) && userIsAdmin())
     deleteQuoteLast();
-  else if (message.content.startsWith(buildTrigger(CONFIG_COMMAND.delete) + ' ') && userIsAdmin())
-    deleteQuote(message.content.replace(`${buildTrigger(CONFIG_COMMAND.delete)} `, ''));
-  else if (message.content === buildTrigger(CONFIG_COMMAND.help) || (message.mentions.members.has(getClient().user.id) || null))
+  else if (message.content.startsWith(buildTrigger(CONFIG_COMMANDS.delete) + ' ') && userIsAdmin())
+    deleteQuote(message.content.replace(`${buildTrigger(CONFIG_COMMANDS.delete)} `, ''));
+  else if (message.content === buildTrigger(CONFIG_COMMANDS.help) || (message.mentions.members.has(getClient().user.id) || null))
     sendHelp();
   else if (message.content === buildTrigger('ping'))
     sendPong();
