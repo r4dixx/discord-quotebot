2157f47c64b7cbf231f90e110e590d531111307c
r4dixx <amael.sikel@gmail.com>
2020-12-01 12:13:20 +0100

    Fix incorrect feedback + const location

--- triggersAdmin.js
+++ triggersAdmin.js
@@ -4,25 +4,25 @@ require('./dbQueries.js')();
 
 module.exports = function() {
 
-  const CONFIG = require('./config.json');
-
-  const CONFIG_FEEDBACK = CONFIG.feedback;
-
-  const CONFIG_FEEDBACK_SUCCESS = CONFIG_FEEDBACK.success;
-  const CONFIG_FEEDBACK_SUCCESS_UPDATE = CONFIG_FEEDBACK_SUCCESS.update;
-
-  const CONFIG_FEEDBACK_ERROR = CONFIG_FEEDBACK.error;
-  const CONFIG_FEEDBACK_ERROR_UPDATE = CONFIG_FEEDBACK_ERROR.update;
-  const CONFIG_FEEDBACK_ERROR_DELETE = CONFIG_FEEDBACK_ERROR.delete;
-
   getClient().on('message', (message) => {
+
+    const CONFIG = require('./config.json');
+
+    const CONFIG_FEEDBACK = CONFIG.feedback;
+
+    const CONFIG_FEEDBACK_SUCCESS = CONFIG_FEEDBACK.success;
+    const CONFIG_FEEDBACK_SUCCESS_UPDATE = CONFIG_FEEDBACK_SUCCESS.update;
+
+    const CONFIG_FEEDBACK_ERROR = CONFIG_FEEDBACK.error;
+    const CONFIG_FEEDBACK_ERROR_UPDATE = CONFIG_FEEDBACK_ERROR.update;
+    const CONFIG_FEEDBACK_ERROR_DELETE = CONFIG_FEEDBACK_ERROR.delete;
 
     updateQuoteItem = function(array) {
       let quoteCurrent = array.shift();
       let quoteNew = array.pop();
       dbUpdateItem(quoteCurrent, quoteNew).then(function(result) {
         if (result == null) message.channel.send(CONFIG_FEEDBACK_ERROR.generic);
-        else if (result == false) message.channel.send(CONFIG_FEEDBACK_ERROR_UPDATE.last);
+        else if (result == false) message.channel.send(CONFIG_FEEDBACK_ERROR_UPDATE.item);
         else message.channel.send(`${CONFIG_FEEDBACK_SUCCESS_UPDATE.title}\n${CONFIG_FEEDBACK_SUCCESS_UPDATE.old}\n${result}\n${CONFIG_FEEDBACK_SUCCESS_UPDATE.new}\n${quoteNew}`);
       });
     };
