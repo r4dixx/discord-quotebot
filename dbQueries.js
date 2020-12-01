3e9d17fe586bdde5012dfd16c89735058bb56f2a
r4dixx <amael.sikel@gmail.com>
2020-12-01 15:04:30 +0100

    Cleanup if statements

--- dbQueries.js
+++ dbQueries.js
@@ -29,7 +29,8 @@ module.exports = function() {
             resolve("error-found");
           } else resolve("error");
           return console.error(errorMessage);
-        } else resolve("success");
+        }
+        resolve("success");
       });
       dbClose();
     });
@@ -42,13 +43,13 @@ module.exports = function() {
         if (err) {
           resolve("error");
           return console.error(err.message);
-        } else if (row == null || row.quote == null) {
-          console.log('Cannot get random quote. No quote found in database');
-          resolve("error-not-found");
-        } else {
-          console.log(`Quote to be displayed: ${row.quote}`);
-          resolve(row.quote);
         }
+        if (row == null || row.quote == null) {
+          resolve("error-not-found");
+          return console.error('Cannot get random quote. No quote found in database');
+        }
+        console.log(`Quote to be displayed: ${row.quote}`);
+        resolve(row.quote);
       });
       dbClose();
     });
@@ -61,16 +62,16 @@ module.exports = function() {
         if (err) {
           resolve("error");
           return console.error(err.message);
-        } else if (row == null || row.quote == null) {
-          console.log(`Error: Cannot get quote for edition. Not found in database: ${quoteCurrent}`);
-          resolve("error-not-found");
-        } else {
-          dbGet().run('UPDATE quotes SET quote = ? WHERE quote = ?', quoteNew, quoteCurrent, function(err) {
-            if (err) return console.error(err.message);
-            console.log(`Updated quote. FROM: ${quoteCurrent} TO: ${quoteNew}`);
-          });
-          resolve("success");
         }
+        if (row == null || row.quote == null) {
+          resolve("error-not-found");
+          return console.error(`Error: Cannot get quote for edition. Not found in database: ${quoteCurrent}`);
+        }
+        dbGet().run('UPDATE quotes SET quote = ? WHERE quote = ?', quoteNew, quoteCurrent, function(err) {
+          if (err) return console.error(err.message);
+          console.log(`Updated quote. FROM: ${quoteCurrent} TO: ${quoteNew}`);
+        });
+        resolve("success");
       });
       dbClose();
     });
@@ -83,17 +84,17 @@ module.exports = function() {
         if (err) {
           resolve("error");
           return console.error(err.message);
-        } else if (row == null || row.quote == null) {
-          console.log('Error: Cannot get last quote for edition. No quote found in database.');
-          resolve("error-not-found");
-        } else {
-          quoteOld = row.quote;
-          dbGet().run('UPDATE quotes SET quote = ? WHERE rowid = (SELECT MAX(rowid) FROM quotes)', quoteNew, function(err) {
-            if (err) return console.error(err.message);
-            console.log(`Updated last quote. FROM: ${quoteOld} TO: ${quoteNew}`);
-          });
-          resolve(quoteOld);
         }
+        if (row == null || row.quote == null) {
+          resolve("error-not-found");
+          return console.error('Error: Cannot get last quote for edition. No quote found in database.');
+        }
+        quoteOld = row.quote;
+        dbGet().run('UPDATE quotes SET quote = ? WHERE rowid = (SELECT MAX(rowid) FROM quotes)', quoteNew, function(err) {
+          if (err) return console.error(err.message);
+          console.log(`Updated last quote. FROM: ${quoteOld} TO: ${quoteNew}`);
+        });
+        resolve(quoteOld);
       });
       dbClose();
     });
@@ -106,13 +107,13 @@ module.exports = function() {
         if (err) {
           resolve("error");
           return console.error(err.message);
-        } else if (row == null || row.quote == null) {
-          console.log(`Error: Cannot get quote for deletion. Not found in database: ${quote}`);
-          resolve("error-not-found");
-        } else {
-          dbDelete(quote);
-          resolve("success");
         }
+        if (row == null || row.quote == null) {
+          resolve("error-not-found");
+          return console.error(`Error: Cannot get quote for deletion. Not found in database: ${quote}`);
+        }
+        dbDelete(quote);
+        resolve("success");
       });
       dbClose();
     });
@@ -122,14 +123,16 @@ module.exports = function() {
     return new Promise(function(resolve, reject) {
       dbOpen();
       dbGet().get('SELECT rowid, quote FROM quotes ORDER BY rowid DESC LIMIT 1', (err, row) => {
-        if (err) return console.error(err.message);
-        if (row == null || row.quote == null) {
-          console.log(`Error: Cannot get last quote for deletion. No quote found in database.`);
-          resolve("error-not-found");
-        } else {
-          dbDelete(row.quote);
-          resolve(row.quote);
+        if (err) {
+          resolve("error");
+          return console.error(err.message);
         }
+        if (row == null || row.quote == null) {
+          resolve("error-not-found");
+          return console.error('Error: Cannot get last quote for deletion. No quote found in database.');
+        }
+        dbDelete(row.quote);
+        resolve(row.quote);
       });
       dbClose();
     });
