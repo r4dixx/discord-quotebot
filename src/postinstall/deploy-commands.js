6041d7dc79b3cea260fe9fba96b209ff38f37d71
r4dixx <amael.sikel@gmail.com>
2021-10-11 15:57:02 +0200

    Move deploy script

--- src/postinstall/deploy-commands.js
+++ src/postinstall/deploy-commands.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { client, token } = require('./config/private.json');
+const { client, token } = require('../config/private.json');
 
 const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!')
