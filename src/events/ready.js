be669244f13e0227eadb7ccde82691be3e8e75a5
r4dixx <amael.sikel@gmail.com>
2021-10-14 14:35:26 +0200

    Fix ready event

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,11 +1,11 @@
 require('../database/dbQueries.js')();
 
 module.exports = {
-	name: 'client-ready',
+	name: 'ready',
 	once: true,
 	execute(client) {
 		const { user } = client;
-		// TODO fix logs not showing
+		// TODO fix logs not
 		console.log(`Discord client ready! Logged in as ${user.username} - ID: ${user.id}`);
 		dbCreateTableIfNecessary();
 	},
