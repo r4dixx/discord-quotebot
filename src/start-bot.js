304a4a80d83eab07cd84d3fbeb7839628736888e
r4dixx <amael.sikel@gmail.com>
2021-10-14 14:39:51 +0200

    Create basic interactionCreate

--- src/start-bot.js
+++ src/start-bot.js
@@ -30,7 +30,11 @@ const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'
 
 for (const file of eventFiles) {
 	const event = require(`./events/${file}`);
-	client.on(event.name, event.execute);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
 }
 
 client.login(process.env.TOKEN);
