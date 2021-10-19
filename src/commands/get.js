cd27d42a124db9b6de56235ea27725019ad5616f
r4dixx <amael.sikel@gmail.com>
2021-10-19 10:14:23 +0200

    Improve randomness approach

--- src/commands/get.js
+++ src/commands/get.js
@@ -12,15 +12,29 @@ module.exports = {
 		const { reply } = get
 		try {
 
+			// As weird as it seems this is how you get a random document from a collection
+			// https://stackoverflow.com/a/46801925/8053848
+
 			const db = require('firebase-admin/firestore').getFirestore()
 			const collection = db.collection(process.env.COLLECTION_NAME)
+			
 			const randomId = collection.doc().id
-			const snapshot = await collection.where("id", ">=", randomId).orderBy("id", "asc").limit(1).get()
-			let data
-			await snapshot.forEach(doc => { data = doc.data() });	
+			console.log(`Generated random id ${randomId}`)
 
-			console.log(randomId)
-			console.log(data)
+			async function getRandomSnapshot(comparator, sorter) {
+				console.log(`Getting random snapshot with ${comparator} comparison and ${sorter} sorting`)
+				var randomSnapshot = await collection.where("id", comparator, randomId).orderBy("id", sorter).limit(1).get()
+				return randomSnapshot
+			}
+
+			var snapshot = await getRandomSnapshot("<=", "desc")
+			if (snapshot.empty) {
+				console.log(chalk.yellow('Snapshot empty, trying the other direction'))
+				snapshot = await getRandomSnapshot(">=", "asc")
+			}
+			
+			let data
+			snapshot.forEach(doc => { data = doc.data() });
 
 			if (snapshot.empty || data.text === undefined) {
 				console.warn(chalk.yellow('Cannot get random quote. No quote found in database'))
